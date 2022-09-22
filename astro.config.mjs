09b86e13c59ce1de0769d034fc52f0729d8f0fab
Jech <j3re.ch0@gmail.com>
2022-09-23 01:19:19 +0800

    added netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
   server: { port: 8080 },
+  output: 'server',
+  adapter: netlify(),
 });
