7e03f62936c518550f026f5da51d00c45705dff6
Jech <j3re.ch0@gmail.com>
2022-10-10 14:31:23 +0800

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify/functions";
-import compress from "astro-compress";
+// import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   server: (command) => ({ port: command === "dev" ? 3333 : 8080 }),
-  integrations: [
-    compress({
-      logger: 2,
-      css: true,
-      html: true,
-      js: true,
-      img: true,
-      svg: true,
-    }),
-  ],
+  // integrations: [
+  //   compress({
+  //     logger: 2,
+  //     css: true,
+  //     html: true,
+  //     js: true,
+  //     img: true,
+  //     svg: true,
+  //   }),
+  // ],
   output: "server",
   adapter: netlify(),
   site: "https://j3rech0.netlify.app",
