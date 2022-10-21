069471a0b312ac8fc15517b8ddc971947ef420d0
Jech <j3re.ch0@gmail.com>
2022-10-21 17:21:26 +0800

    anim

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,15 @@
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify/functions";
-// import compress from "astro-compress";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   server: (command) => ({ port: command === "dev" ? 3333 : 8080 }),
-  // integrations: [
-  //   compress({
-  //     logger: 2,
-  //     css: true,
-  //     html: true,
-  //     js: true,
-  //     img: true,
-  //     svg: true,
-  //   }),
-  // ],
+  integrations: [compress()],
   output: "server",
   adapter: netlify(),
-  site: "https://j3rech0.netlify.app",
+  build: {
+    sitemap: true,
+  },
+  site: "https://jerecho.com",
 });
