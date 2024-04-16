bb76f73f61f3456b0f646c65f8353c29cf3a3dcc
Jech <j3re.ch0@gmail.com>
2024-04-16 11:40:42 +0800

    t

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify/functions";
-import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   server: (command) => ({ port: command === "dev" ? 3333 : 8080 }),
-  integrations: [compress()],
   output: "server",
   adapter: netlify(),
   build: {
