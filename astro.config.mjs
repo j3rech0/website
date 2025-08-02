1771541fbc82975ec2629623a0f40f764874b00a
Jech <j3re.ch0@gmail.com>
2025-08-02 19:12:50 +0800

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify";
+import netlify from "@astrojs/netlify/edge-functions";
 
 // https://astro.build/config
 export default defineConfig({
